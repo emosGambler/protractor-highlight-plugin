4ca9708767cee75f66a412a64d56abb74764189f
Jacek Góraj <jckgrj@gmail.com>
2018-07-31 21:32:01 +0200

    removed unneeded code

---
+++ proj/dist/logs/logs.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function onPageLoad() {
+    this.addSuccess({ specName: 'Hello, World!' });
+}
+exports.onPageLoad = onPageLoad;
+;
+//# sourceMappingURL=logs.js.map
